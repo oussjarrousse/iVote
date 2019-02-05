ef487d95ed46d01f77b05a00cf42dce3c03d8daf
Oussama Jarrousse <oussama@jarrousse.org>
2019-02-05 13:37:45 +0100

    rearranging the project

--- ivote/ivote/settings.py
+++ ivote/ivote/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
+
 #INTEGRATING REACT WITH DJANGO USING WEBPACK LOADER
 WEBPACK_LOADER = {
     'DEFAULT': {
